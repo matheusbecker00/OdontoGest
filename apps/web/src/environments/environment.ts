24de836e56d60863b3a0e61870fa92e8b69c940d
Matheus Becker <matheusbecker484@gmail.com>
2026-07-21 16:12:25 -0300

    Add support WhatsApp number

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   useFirebaseAuthEmulator: false,
   adminEmails: ['matheusbecker484@gmail.com'],
   supportWhatsAppUrl:
-    'https://wa.me/?text=Ola%2C%20quero%20falar%20com%20o%20suporte%20do%20OdontoGest.',
+    'https://wa.me/5547984216946?text=Ola%2C%20quero%20falar%20com%20o%20suporte%20do%20OdontoGest.',
 } as const;
